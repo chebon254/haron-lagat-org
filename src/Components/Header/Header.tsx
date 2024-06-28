101e5c6ed702503935c3d58b66d06b439e2ac6e8
chebon254 <kelvinchebon90@gmail.com>
2024-06-28 18:15:12 +0300

    Menu button fix

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -24,7 +24,7 @@ const Header = () => {
               onClick={handleNav}
               data-collapse-toggle="navbar-default"
               type="button"
-              className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+              className="nav-button inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
               aria-controls="navbar-default"
               aria-expanded="false"
             >
